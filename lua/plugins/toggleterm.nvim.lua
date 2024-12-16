a4b26fa1e9c20ff5799517a1a4430b99f98f24a6
geometriccross <geometriccross@gmail.com>
2024-12-17 00:59:29 +0900

    add toggleterm setting

--- lua/plugins/toggleterm.nvim.lua
+++ lua/plugins/toggleterm.nvim.lua
@@ -7,11 +7,21 @@ return {
 	    direction = "float",
 	    hidden = true
 	})
+	
+	local float_terminal = Terminal:new({
+	    direction = "float",
+	    hidden = true
+	})	
 
 	function _lazygit_toggle()
 	    lazygit:toggle()
 	end
 
-	vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+	function _terminal_toggle()
+	    float_terminal:toggle()
+	end
+
+	vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+	vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>lua _terminal_toggle()<CR>", {noremap = true, silent = true})
     end
-}
+}	
