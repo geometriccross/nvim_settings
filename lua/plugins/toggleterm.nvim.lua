1ff009b39ed2dd3d11954ef7b7c18cb11e01ed0e
geometriccross <geometriccross@gmail.com>
2024-12-19 21:06:04 +0900

    add setup cmd in tail of config

--- lua/plugins/toggleterm.nvim.lua
+++ lua/plugins/toggleterm.nvim.lua
@@ -1,27 +1,30 @@
 return {
-    'akinsho/toggleterm.nvim',
-    config = function()
-	local Terminal  = require('toggleterm.terminal').Terminal
-	local lazygit = Terminal:new({
-	    cmd = "lazygit",
-	    direction = "float",
-	    hidden = true
-	})
-	
-	local float_terminal = Terminal:new({
-	    direction = "float",
-	    hidden = true
-	})	
+	'akinsho/toggleterm.nvim',
+	config = function()
+		local Terminal       = require('toggleterm.terminal').Terminal
+		local lazygit        = Terminal:new({
+			cmd = "lazygit",
+			direction = "float",
+			hidden = true
+		})
 
-	function _lazygit_toggle()
-	    lazygit:toggle()
+		local float_terminal = Terminal:new({
+			direction = "float",
+			hidden = true
+		})
+
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+
+		function _terminal_toggle()
+			float_terminal:toggle()
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>lua _terminal_toggle()<CR>",
+			{ noremap = true, silent = true })
+
+		require('toggleterm').setup()
 	end
-
-	function _terminal_toggle()
-	    float_terminal:toggle()
-	end
-
-	vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
-	vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>lua _terminal_toggle()<CR>", {noremap = true, silent = true})
-    end
-}	
+}
