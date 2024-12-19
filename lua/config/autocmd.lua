b3fb739422bc93d85046add160cfa9f6cfbace48
geometriccross <geometriccross@gmail.com>
2024-12-19 22:11:43 +0900

    change autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -2,29 +2,30 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    callback = function()
-	vim.highlight.on_yank()
-    end,
+	desc = 'Highlight when yanking (copying) text',
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- Run shellspec on save
-vim.api.nvim_create_autocmd('BufWrite', {
-desc = 'run shellspec if spec folder exists',
-pattern = '*.sh',
-callback = function()
-    -- get a current dir
-    local cwd = vim.fn.getcwd()
-    -- check that the specific spec folder is exists
-    if vim.fn.isdirectory(cwd .. '/' .. 'spec') == 1 then
-	local term_bufs = GetTerminalBuffer()
-	if #term_bufs == 0 then
-	    vim.cmd.vsplit()
-	end
-	
-	-- select first terminal buffers
-	local target_buf = term_bufs[1]
-	SendToTerminal(target_buf, 'shellspec')
-    end
-end,
+local term_size = 180
+local Terminal = require("toggleterm.terminal").Terminal
+local term = Terminal:new({
+	direction = "vertical",
+	size = term_size
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+	desc = 'run shellspec if spec folder exists',
+	pattern = '*.sh',
+	callback = function()
+		if not term:is_open() then
+			term:open()
+			term:resize(term_size)
+		end
+
+		term:resize(term_size)
+		term:send('shellspec')
+	end,
 })
