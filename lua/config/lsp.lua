df92baa7dabd6e5869e7be5b995a90287a8cff41
geometriccross <geometriccross@LAPTOP.localdomain>
2025-01-15 16:52:30 +0900

    refactor powershell_es path settings, it can run in linux and windows

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -13,21 +13,21 @@ require('mason').setup {
 
 require('mason-lspconfig').setup_handlers {
 	function(server_name)
-		-- powershell_es setting
-		-- Please check this issue https://github.com/PowerShell/PowerShellEditorServices/issues/2092
-		if server_name == 'powershell_es' then
-			require("lspconfig").powershell_es.setup {
-				filetypes = { "ps1", "psm1", "psd1" },
-				bundle_path = "~/AppData/Local/nvim-data/mason/packages/powershell-editor-services",
-				settings = { powershell = { codeFormatting = { Preset = 'OTBS' } } },
-				init_options = {
-					enableProfileLoading = false,
-				},
-			}
-		else
-			require('lspconfig')[server_name].setup {
-				capabilities = require('cmp_nvim_lsp').default_capabilities(),
-			}
-		end
+		require('lspconfig')[server_name].setup {
+			capabilities = require('cmp_nvim_lsp').default_capabilities(),
+		}
+	end,
+	-- please refer this, https://medium.com/@kacpermichta33/powershell-development-in-neovim-23ed44d453b4
+	powershell_es = function()
+		require("lspconfig").powershell_es.setup {
+			on_attach = function(_, bufnr)
+				vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+			end,
+
+			filetypes = { "ps1", "psm1", "psd1" },
+			-- powershell_es is not provide binaly, so it need to provide the path of the script files
+			bundle_path = vim.fn.expand(vim.fn.stdpath('data') .. '/mason/packages/powershell-editor-services'),
+			settings = { powershell = { codeFormatting = { Preset = 'OTBS' } } },
+		}
 	end
 }
