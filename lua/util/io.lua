8cd683aac1985bc1668f3b6b72b096cb8b3b6b9b
geometriccross <geometriccross@gmail.com>
2025-02-03 14:43:10 +0900

    new function

--- lua/util/io.lua
+++ lua/util/io.lua
@@ -9,3 +9,13 @@ function LibPath(path)
 
 	return vim.fn.expand(vim.fn.stdpath('data') .. path)
 end
+
+function FileExists(name)
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
+end
