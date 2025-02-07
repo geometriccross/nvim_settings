6f6501b85af465e2e1ee48ee37228930b9743628
geometriccross <geometriccross@gmail.com>
2025-02-08 01:13:12 +0900

    add arg in util functions

--- lua/util/io.lua
+++ lua/util/io.lua
@@ -1,13 +1,13 @@
 -- get dir path that has mason, lazy dir
 -- and you can appended passed path
 
-function LibPath(path)
+function LibPath(stdpath, path)
 	-- if passed like this 'hoge/aaa', it need to add / in top of string
 	if string.sub(path, 1, 1) ~= '/' then
 		path = '/' .. path
 	end
 
-	return vim.fn.expand(vim.fn.stdpath('data') .. path)
+	return vim.fn.expand(vim.fn.stdpath(stdpath) .. path)
 end
 
 function FileExists(name)
