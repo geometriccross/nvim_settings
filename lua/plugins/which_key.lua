6dd1d2db3eb5cd7bddbf5ca86b696f7db118bfa7
geometriccross <geometriccross@gmail.com>
2025-02-08 12:04:18 +0900

    Add which key

---
+++ lua/plugins/which_key.lua
@@ -0,0 +1,19 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		triggers = {
+			{ "<auto>", mode = "nixsotc" },
+			{ "a",      mode = { "n", "v" } },
+		}
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
+}
