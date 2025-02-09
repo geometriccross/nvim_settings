7ef8daf2b4ad8a955cae4542a15ec4132b2f6096
geometriccross <geometriccross@LAPTOP.localdomain>
2025-02-09 20:27:17 +0900

    add new plugin for auto pair on lisp langs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,5 +1,16 @@
 return {
-    'windwp/nvim-autopairs',
+	'windwp/nvim-autopairs',
 	event = 'InsertEnter',
 	config = true,
+	-- disable if you write lisp
+	-- in this case, auto pair is done by nvim-parinfer
+	-- check lua/plugins/nvim-parinfer.lua
+	disable_filetype = {
+		"TelescopePrompt",
+		"spectre_panel",
+		"*.rkt",
+		"*.scm",
+		"*.lisp",
+		"*.clj",
+	},
 }
