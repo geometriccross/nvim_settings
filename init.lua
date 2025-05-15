0a52a0eb34644d7ebb78729353acad1d6fdea0c5
geometriccross <geometriccross@desktop.>
2025-05-16 01:41:58 +0900

    change a init.lua to combinate vscode and neovim

--- init.lua
+++ init.lua
@@ -1,4 +1,24 @@
 require 'config.common.lazy'
-require 'config.common.combinator'
+
+local plugins = {
+    { import = "config.common.plugins" },
+    { import = "colorscheme" }
+}
+if vim.g.vscode then
+    -- table.insert(plugins, { import = "config.vscode.plugins" })
+else
+    table.insert(plugins, { import = "config.neovim.plugins" })
+end
+SetupLazy(plugins)
+
+require 'config.common.autocmd'
+require 'config.common.keymaps'
+require 'config.common.settings'
+if vim.g.vscode then
+    -- vscode specific settings
+else
+    require 'config.neovim.autocmd'
+    require 'config.neovim.cmp'
+end
 
 vim.cmd('colorscheme kanagawa-dragon')
