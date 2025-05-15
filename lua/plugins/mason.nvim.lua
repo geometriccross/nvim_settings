4af73422225f4a343cfa6e440704daa82dc58e50
geometriccross <geometriccross@desktop.>
2025-05-15 12:26:07 +0900

    while this issue 'https://github.com/mason-org/mason.nvim/issues/1929' will close, chang plugin repoes

--- lua/plugins/mason.nvim.lua
+++ lua/plugins/mason.nvim.lua
@@ -1,7 +1,7 @@
 return {
-	"williamboman/mason.nvim",
+	{ "mason-org/mason.nvim", version = "^1.0.0" },
+	{ "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
 		'hrsh7th/cmp-nvim-lsp',
 	},
