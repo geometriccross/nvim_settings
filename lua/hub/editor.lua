afa7b50cf448f63afd0329abe0236dac11f00034
geometriccross <geometriccross@desktop.>
2025-05-15 16:46:42 +0900

    add ignore list

--- lua/hub/editor.lua
+++ lua/hub/editor.lua
@@ -27,7 +27,7 @@ local function plugin_path()
 end
 
 local function get_plugins()
-        local module_table = {}
+    local module_table = {}
     for _, entry in ipairs(scandir(plugin_path())) do
         table.insert(module_table, require("plugins." .. remove_extension(entry.name)))
     end
@@ -35,9 +35,51 @@ local function get_plugins()
     return module_table
 end
 
+local function ignore_plugin(name, tbl)
+    local result = {}
+    for _, v in ipairs(tbl) do
+        if v.name ~= name then
+            return table.insert(result, v)
+        end
+    end
+    return result
+end
+
+local function ignore_plugins(names, tbl)
+    result = {}
+    for _, value in ipairs(tbl) do
+        for _, name in ipairs(names) do
+            if value.name ~= name then
+                table.insert(result, value)
+            end
+        end
+    end
+    return result
+end
+
 if vim.g.vscode then
     print("Running in VSCode")
-    return {}
+    local plg = get_plugins()
+    -- specify the file name of plugin
+    local ignore_list = {
+        "avante",
+        "gitsigns",
+        "lualine",
+        "mason",
+        "nvim-autopairs",
+        "nvim-cmp",
+        "nvim-lspconfig",
+        "nvim-parinfer",
+        "nvim-tree",
+        "nvim-treesitter",
+        "nvim-treesitter-endwise",
+        "telescope",
+        "toggleterm",
+        "which_key",
+    }
+
+    print("Ignoring plugins: " .. ignore_plugins(ignore_list, plg))
+    return ignore_plugins(ignore_list, plg)
 else
     print("Running in Neovim")
     return get_plugins()
