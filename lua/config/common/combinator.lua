563270139e87b4e559291c9cd4c920b4db58297a
geometriccross <geometriccross@desktop.>
2025-05-16 00:28:35 +0900

    delete unused file

--- lua/config/common/combinator.lua
+++
@@ -1,9 +0,0 @@
-require 'config.common.settings'
-require 'config.common.keymaps'
-require 'config.common.autocmd'
-
-if not vim.g.vscode then
-    require 'config.cmp'
-    require 'lsp.mason'
-    require 'lsp.racket'
-end
