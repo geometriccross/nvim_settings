6d0fb14b2fd0c1d3238116c2aa0f40094381e3f2
geometriccross <geometriccross@desktop.>
2025-05-15 22:38:31 +0900

    add condition for vscode-neovim

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -8,13 +8,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	end,
 })
 
--- Run shellspec on save
-local term_size = 40
-local Terminal = require("toggleterm.terminal").Terminal
-local term = Terminal:new({
-	direction = "vertical",
-	size = term_size
-})
+if not vim.g.vscode then
+	-- Run shellspec on save
+	local term_size = 40
+	local Terminal = require("toggleterm.terminal").Terminal
+	local term = Terminal:new({
+		direction = "vertical",
+		size = term_size
+	})
+end
 
 --- Check if a file or directory exists in this path
 local function exists(file)
