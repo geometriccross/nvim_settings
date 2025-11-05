5ca610e7fc9a251724630b12dd927680eef63545
geometriccross <geometriccross@desktop.>
2025-11-06 01:14:21 +0900

    add treesj

---
+++ lua/config/common/plugins/treesj.lua
@@ -0,0 +1,9 @@
+return {
+	'Wansmer/treesj',
+	event = 'InsertEnter',
+	keys = { '<leader>m', '<leader>j', '<leader>s' },
+	dependencies = { 'nvim-treesitter/nvim-treesitter' },
+	config = function()
+		require('treesj').setup({})
+	end
+}
