1ddde7efcb0fd49cffd62c525f640f61575727f5
geometriccross <geometriccross@desktop.>
2025-11-06 00:53:09 +0900

    change a lualine

--- lua/config/neovim/plugins/lualine.lua
+++ lua/config/neovim/plugins/lualine.lua
@@ -1,4 +1,12 @@
 return {
 	'nvim-lualine/lualine.nvim',
-	config = true,
+	config = function()
+		require('lualine').setup({
+			options = {
+				theme = 'gruvbox-material',
+				section_separators = { left = '', right = '' },
+				component_separators = { left = '', right = '' },
+			},
+		})
+	end
 }
