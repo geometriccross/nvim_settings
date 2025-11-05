92a8a5064da1a3d8216fb3ee3b0603eb00ff5b42
geometriccross <geometriccross@desktop.>
2025-11-06 01:14:15 +0900

    modify focus.nvim

--- lua/config/neovim/plugins/focus.lua
+++ lua/config/neovim/plugins/focus.lua
@@ -2,5 +2,17 @@ return {
 	"nvim-focus/focus.nvim",
 	config = function()
 		require("focus").setup()
+		local ignore_buftypes = { 'nofile', 'prompt', 'popup', 'CodeCompanion', 'NvimTree' }
+
+		local augroup =
+			vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+		vim.api.nvim_create_autocmd('WinEnter', {
+			group = augroup,
+			callback = function(_)
+				vim.w.focus_disable = vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+			end,
+			desc = 'Disable focus autoresize for BufType',
+		})
 	end
 }
