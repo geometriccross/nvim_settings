ad01c76d26955ff70d4e99ae058302247c0237d3
geometriccross <geometriccross@LAPTOP.localdomain>
2026-01-06 13:40:52 +0900

    fix focus setting, add filetype ignore

--- lua/config/neovim/plugins/focus.lua
+++ lua/config/neovim/plugins/focus.lua
@@ -2,16 +2,19 @@ return {
 	"nvim-focus/focus.nvim",
 	config = function()
 		require("focus").setup()
-		local ignore_buftypes = { "nofile", "prompt", "popup", "CodeCompanion", "NvimTree" }
+		local ignore_buftypes = { "nofile", "prompt", "popup" }
+		local ignore_filetypes = { "CodeCompanion", "NvimTree" }
 
 		local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
 
 		vim.api.nvim_create_autocmd("WinEnter", {
 			group = augroup,
 			callback = function(_)
-				vim.w.focus_disable = vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+				local buftype_match = vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+				local filetype_match = vim.tbl_contains(ignore_filetypes, vim.bo.filetype)
+				vim.w.focus_disable = buftype_match or filetype_match
 			end,
-			desc = "Disable focus autoresize for BufType",
+			desc = "Disable focus autoresize for BufType and FileType",
 		})
 	end,
 }
