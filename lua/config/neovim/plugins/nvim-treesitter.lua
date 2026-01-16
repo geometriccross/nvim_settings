07a413b43ad462f5884bf9f2b7b566305e1ec61f
geometriccross <geometriccross@geometriccrossnoMacBook-Air.local>
2026-01-16 23:44:33 +0900

    add langs in ensure installed

--- lua/config/neovim/plugins/nvim-treesitter.lua
+++ lua/config/neovim/plugins/nvim-treesitter.lua
@@ -4,6 +4,26 @@ return {
 	build = ":TSUpdate",
 	dependencies = { "RRethy/nvim-treesitter-endwise" },
 	config = function()
-		require 'nvim-treesitter'.install { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" }
+		require 'nvim-treesitter'.install {
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"markdown",
+			"markdown_inline",
+			"css",
+			"html",
+			"javascript",
+			"latex",
+			"norg",
+			"scss",
+			"svelte",
+			"tsx",
+			"typst",
+			"vue",
+			"python",
+			"bash",
+			"zsh"
+		}
 	end,
 }
